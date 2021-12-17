c3b67a973d3938587d363cc98cba7d8fc068a989
Sourabh Saraswat <saraswatsourabh5@gmail.com>
2021-12-17 23:00:00 +0530

    Test file update
    Signed-off-by: Sourabh Saraswat <saraswatsourabh5@gmail.com>

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-marks = 10000
-  
-# perform division with 0
-a = marks / 0
-print(a)
+class complex:
+    def __init__(self, a):
+        self.a = a
+ 
+     # adding two objects
+    def __sub__(self, other):
+        return self.a * other.a
+ 
+Ob1 = complex(2)
+Ob2 = complex(6)
+Ob3 = Ob2 - Ob1
+print(Ob3)
